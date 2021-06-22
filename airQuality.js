e7c270212d17196e4b6f070974d9e80f7d713af1
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-22 17:58:54 +0200

    sorted air quality levels by half-year

--- airQuality.js
+++ airQuality.js
@@ -12,11 +12,23 @@ axios.get('https://data.cityofnewyork.us/resource/c3uy-2p5r.json')
       //   ref[place] = true
       // }
       if (place === 'Chelsea-Village') {
-        chels[res.time_period] = res.data_value
+        if (!res.time_period.includes('Annual')) {
+          chels[res.time_period] = res.data_value
+        }
       }
     }
+    var keys = Object.keys(chels)
 
-   console.log('chels: ', chels)
+    var sorted = keys.sort((a, b) => {
+
+      var aNum = a.includes('Winter') ? Number(a.split(' ')[1].split('-')[0]) : Number(a.split(' ')[1])
+      var bNum = b.includes('Winter') ? Number(b.split(' ')[1].split('-')[0]) : Number(b.split(' ')[1])
+      console.log(aNum, bNum)
+      return aNum - bNum
+
+    })
+
+    console.log('chels: ', sorted)
   })
   .catch((err) => {
     console.log('errr: ', err)
