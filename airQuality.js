ebad7eb29ebf5f6c31d211d6230e91e04b7914ae
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-23 17:54:23 +0200

    API call to harbor water quality

--- airQuality.js
+++ airQuality.js
@@ -7,28 +7,30 @@ axios.get('https://data.cityofnewyork.us/resource/c3uy-2p5r.json')
     for (var i = 0; i < response.data.length; i++) {
       var res = response.data[i]
       var place = response.data[i].geo_place_name
-      // console.log(place)
-      // if (!ref[place]) {
-      //   ref[place] = true
-      // }
+
       if (place === 'Chelsea-Village') {
         if (!res.time_period.includes('Annual')) {
           chels[res.time_period] = res.data_value
         }
       }
     }
+
+    console.log(chels)
     var keys = Object.keys(chels)
 
     var sorted = keys.sort((a, b) => {
 
       var aNum = a.includes('Winter') ? Number(a.split(' ')[1].split('-')[0]) : Number(a.split(' ')[1])
       var bNum = b.includes('Winter') ? Number(b.split(' ')[1].split('-')[0]) : Number(b.split(' ')[1])
-      console.log(aNum, bNum)
-      return aNum - bNum
+      return aNum - bNum;
 
     })
 
-    console.log('chels: ', sorted)
+    var levels = []
+    for (var i = 0; i < sorted.length; i ++) {
+      levels.push([sorted[i], Math.round ( Number(chels[sorted[i]])) ]  )
+    }
+    console.log(levels)
   })
   .catch((err) => {
     console.log('errr: ', err)
