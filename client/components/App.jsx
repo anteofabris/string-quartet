81d890b48634965fb6af99596695acc7709452cc
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-29 22:49:01 +0200

    code cleanup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,24 +7,6 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    // const something = new Tone.Synth().toDestination()
-    // const noise = new Tone.Noise({
-    // 	volume: -10,
-    // 	type: "brown"
-    // }).toDestination();
-
-    // const toneWaveform = new Tone.Waveform();
-    // noise.connect(toneWaveform);
-    // waveform({
-    // 	parent: document.querySelector("#content"),
-    // 	tone: toneWaveform,
-    // });
-
-    // ui({
-    // 	parent: document.querySelector("#content"),
-    // 	tone: noise
-    // });
-
     this.create = this.create.bind(this)
   }
 
@@ -34,9 +16,6 @@ class App extends React.Component {
   }
 
   create() {
-    // console.log('audoContext:', context)
-    // StartAudioContext(context, "#playButton")
-    // noise.start()
 
     var osc = new Tone.Oscillator()
     osc.connect(Tone.Master)
