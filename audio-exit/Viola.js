549baf74c6610d0cb75767574d177529e4bfb558
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-30 03:24:45 +0200

    updated file structure

---
+++ audio-exit/Viola.js
@@ -0,0 +1,6 @@
+// import * as Tone from 'tone';
+const Tone = require('tone')
+
+module.exports.play = () => {
+
+}
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,18 +11,21 @@ class App extends React.Component {
       samples: {}
     }
     this.create = this.create.bind(this)
+    this.play = this.play.bind(this)
   }
 
   componentDidMount() {
-    console.log('mountedn')
     axios
-    .get('/samples')
-    .then((res) => {
-      console.log('got res! ', res)
-    })
-    .catch((err) => {
-      console.log('err getting data! ', err)
-    })
+      .get('/samples')
+      .then((res) => {
+        console.log('got res! ', res)
+        this.setState({
+          samples: res.data
+        })
+      })
+      .catch((err) => {
+        console.log('err getting data! ', err)
+      })
 
   }
 
@@ -34,18 +37,27 @@ class App extends React.Component {
 
   }
 
-  play () {
-
+  play() {
+    console.log('operations on first date: ', Object.keys(this.state.samples)[0])
     // this needs to be async
     // create a day variable set to earliest date in object
-    // create a setInterval function, update every 100 (for now) milliseconds
+    var day = new Date('1995-12-17T00:00:00')
+    console.log('this is day: ', day)
+
+    // create a recursive function with a setTimeOut function
+    // if day exists in this.samples
+    // play the values with toneJS
+    // call recursive function after a setTimeOut of 10ms (for now)
+    // if it is the last day
+    // return console.log('done!')
   }
 
   render() {
     return (
       <div>
         <h1>App.js is connected and working!</h1>
-        <button id="playButton" onClick={this.create}>Clicks</button>
+        <button id="createButton" onClick={this.create}>Clicks</button>
+        <button id="playButton" onClick={this.play}>PLAY</button>
         <Cats />
       </div>
     )
